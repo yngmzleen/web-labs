55cdec612b01a786b3ec676cad3401c802c23882
Иванов Егор <ivnvegor@gmail.com>
2024-09-01 21:08:28 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -3,6 +3,9 @@ function showTime() {
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
     document.getElementById('time2').innerHTML = currentTime;
+
+    document.getElementById('timeAnalog').style.transform = `rotate(${180 + today.getSeconds() * 6 + (today.getMilliseconds() * 0.006)}deg) translate(0, 50px)`;
+
 }
 
 setInterval(showTime,1)
